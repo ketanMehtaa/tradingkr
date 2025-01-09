68068a26b527861983fde79312ce3788526b851e
ketan mehta <meetketanmehta@gmail.com>
2025-01-09 18:31:53 +0530

    change remoe '

--- src/app/news/not-found.tsx
+++ src/app/news/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="text-center">
       <h2 className="text-2xl font-bold mb-4">Article Not Found</h2>
-      <p className="mb-4">Sorry, the article you're looking for doesn't exist.</p>
+      <p className="mb-4">Sorry, the article youre looking for doesnt exist.</p>
       <Link href="/news" className="text-blue-500 hover:underline">
         Back to News
       </Link>
